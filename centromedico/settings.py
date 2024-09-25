b40aba5083170315f73db3c01ec1e73d327aef06
jorgealbertmora <jorge.albert.ceb@immune.institute>
2024-09-25 11:43:36 +0200

    solucion

--- centromedico/settings.py
+++ centromedico/settings.py
@@ -28,7 +28,7 @@ FERNET_KEY = config('FERNET_KEY')
 FIELD_ENCRYPTION_KEY = config('ENCRYPTED_MODEL_FIELDS_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['centromedicoimmune.onrender.com', 'localhost', '127.0.0.1']
 
@@ -125,10 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Enable WhiteNoise's GZip compression and caching support
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
