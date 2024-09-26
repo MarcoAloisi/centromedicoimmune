651fadb8eb82e8fdbeacdaec401acc1a44b755cd
MarcoAloisi <131871979+MarcoAloisi@users.noreply.github.com>
2024-09-26 11:30:46 +0200

    actualizacion del models, views, inicio de sesions

--- centromedico/settings.py
+++ centromedico/settings.py
@@ -99,6 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -107,6 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
